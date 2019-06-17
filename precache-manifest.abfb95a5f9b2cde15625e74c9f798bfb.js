3b626044e4e153b5f2de8965952097cba30d2f35
Arkadiusz Plumbaum <arekpl28@wp.pl>
2019-06-17 20:57:52 +0200

    Updates

---
+++ precache-manifest.abfb95a5f9b2cde15625e74c9f798bfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b81ac59d4aed8c3c83751ba51bc28a",
+    "url": "/projectpcss/index.html"
+  },
+  {
+    "revision": "c07803adc9e6e305e033",
+    "url": "/projectpcss/static/css/main.7f35bfd9.chunk.css"
+  },
+  {
+    "revision": "89c67045c67b3d8344df",
+    "url": "/projectpcss/static/js/2.be3c7d22.chunk.js"
+  },
+  {
+    "revision": "c07803adc9e6e305e033",
+    "url": "/projectpcss/static/js/main.2269f5cc.chunk.js"
+  },
+  {
+    "revision": "7dedf2ad82d7d0c04c10",
+    "url": "/projectpcss/static/js/runtime~main.ac305640.js"
+  }
+]);
